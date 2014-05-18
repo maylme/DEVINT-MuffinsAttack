dd076e5beb51b9c2e1c0bb523a17806cf06d9742
Maylanie MESNIER <lyameina@gmail.com>
2014-05-18 18:11:31 +0200

    Test des niveaux et utilisateurs

--- src/test/global/difficultes/NiveauTest.java
+++ src/test/global/difficultes/NiveauTest.java
@@ -1,36 +1,118 @@
 package test.global.difficultes;
 
+import jeu.global.difficultes.Niveau;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Mayl on 13/05/2014.
  */
 public class NiveauTest {
-    @org.junit.Test
+
+    private Niveau n;
+    @Before
+    public void setUp(){
+        n = Niveau.UN;
+    }
+
+
+    @Test
+    public void testGetName(){
+        assertEquals( n.getName(), "UN");
+        n =  n.suivant();
+        assertEquals( n.getName(), "DEUX");
+        n =  n.suivant();
+        assertEquals( n.getName(), "TROIS");
+        n =  n.suivant();
+        assertEquals( n.getName(), "QUATRE");
+        n =  n.suivant();
+        assertEquals( n.getName(), "CINQ");
+        n =  n.suivant();
+        assertEquals( n.getName(), "CINQ");
+    }
+
+    @Test
     public void testGetNbLettres() throws Exception {
+        assertEquals( n.getNbLettres(), 5);
+        n =  n.suivant();
+        assertEquals( n.getNbLettres(), 10);
+        n =  n.suivant();
+        assertEquals( n.getNbLettres(), 15);
+        n =  n.suivant();
+        assertEquals( n.getNbLettres(), 20);
+        n =  n.suivant();
+        assertEquals( n.getNbLettres(), 26);
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetAlphabet() throws Exception {
+        assertEquals( n.getAlphabet(), "ABCDE");
+        n =  n.suivant();
+        assertEquals( n.getAlphabet(), "ABCDEFGHIJ");
+        n =  n.suivant();
+        assertEquals( n.getAlphabet(), "ABCDEFGHIJKLMNO");
+        n =  n.suivant();
+        assertEquals( n.getAlphabet(), "ABCDEFGHIJKLMNOPQRST");
+        n =  n.suivant();
+        assertEquals( n.getAlphabet(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     }
 
-    @org.junit.Test
+    @Test
     public void testGetObjectif() throws Exception {
-
+        assertEquals( n.getObjectif(), 30);
+        n =  n.suivant();
+        assertEquals( n.getObjectif(), 60);
+        n =  n.suivant();
+        assertEquals( n.getObjectif(), 90);
+        n =  n.suivant();
+        assertEquals( n.getObjectif(), 150);
+        n =  n.suivant();
+        assertEquals( n.getObjectif(), 200);
     }
 
-    @org.junit.Test
+    @Test
     public void testGetById() throws Exception {
-
+        assertEquals( Niveau.getById(1), n);
+        n =  n.suivant();
+        assertEquals( Niveau.getById(2), n);
+        n =  n.suivant();
+        assertEquals( Niveau.getById(3), n);
+        n =  n.suivant();
+        assertEquals( Niveau.getById(4), n);
+        n =  n.suivant();
+        assertEquals( Niveau.getById(5), n);
     }
 
-    @org.junit.Test
+    @Test
     public void testToString() throws Exception {
+        assertEquals( n.toString(), "1");
+        n =  n.suivant();
+        assertEquals( n.toString(), "2");
+        n =  n.suivant();
+        assertEquals( n.toString(), "3");
+        n =  n.suivant();
+        assertEquals( n.toString(), "4");
+        n =  n.suivant();
+        assertEquals( n.toString(), "5");
 
     }
 
-    @org.junit.Test
+    @Test
     public void testSuivant() throws Exception {
-
+        assertEquals( n.getName(), "UN");
+        n =  n.suivant();
+        assertEquals( n.getName(), "DEUX");
+        n =  n.suivant();
+        assertEquals( n.getName(), "TROIS");
+        n =  n.suivant();
+        assertEquals( n.getName(), "QUATRE");
+        n =  n.suivant();
+        assertEquals( n.getName(), "CINQ");
+        n =  n.suivant();
+        assertEquals( n.getName(), "CINQ");
     }
 }
--- src/test/global/voix/UtilisateurTest.java
+++ src/test/global/voix/UtilisateurTest.java
@@ -1,7 +1,17 @@
 package test.global.voix;
 
 import jeu.global.Utilisateur;
+import jeu.global.couleurs.Couleurs;
+import jeu.global.difficultes.Niveau;
+import junit.framework.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Mayl on 13/05/2014.
@@ -12,6 +22,70 @@ public class UtilisateurTest {
 
     @Before
     public void setUp(){
-        u = new Utilisateur ();
+        u = new Utilisateur ("Utilisateur_Test");
     }
+
+    @Test
+    public void getCouleursChoisiesTest(){
+
+        //par defaut:
+        assertEquals(u.getCouleursChoisies(), Couleurs.BLEUBLANC);
+    }
+
+    @Test
+    public void getNiveauTest(){
+        assertEquals(u.getNiveau(), Niveau.UN);
+
+        u.setNiveau(Niveau.DEUX);
+        assertEquals(u.getNiveau(), Niveau.DEUX);
+
+        u.setNiveau(Niveau.TROIS);
+        assertEquals(u.getNiveau(), Niveau.TROIS);
+
+        u.setNiveau(Niveau.QUATRE);
+        assertEquals(u.getNiveau(), Niveau.QUATRE);
+
+        u.setNiveau(Niveau.CINQ);
+        assertEquals(u.getNiveau(), Niveau.CINQ);
+    }
+
+    @Test
+    public void niveauSuivantTest(){
+        assertEquals(u.getNiveau(), Niveau.UN);
+
+        u.niveauSuivant();
+        assertEquals(u.getNiveau(), Niveau.DEUX);
+
+        u.niveauSuivant();
+        assertEquals(u.getNiveau(), Niveau.TROIS);
+
+        u.niveauSuivant();
+        assertEquals(u.getNiveau(), Niveau.QUATRE);
+
+        u.niveauSuivant();
+        assertEquals(u.getNiveau(), Niveau.CINQ);
+
+        u.niveauSuivant();
+        assertEquals(u.getNiveau(), Niveau.CINQ);
+    }
+
+
+    @Test
+    public void setNiveauTest(){
+        assertEquals(u.getNiveau(), Niveau.UN);
+
+        u.setNiveau(Niveau.DEUX);
+        assertEquals(u.getNiveau(), Niveau.DEUX);
+
+        u.setNiveau(Niveau.TROIS);
+        assertEquals(u.getNiveau(), Niveau.TROIS);
+
+        u.setNiveau(Niveau.QUATRE);
+        assertEquals(u.getNiveau(), Niveau.QUATRE);
+
+        u.setNiveau(Niveau.CINQ);
+        assertEquals(u.getNiveau(), Niveau.CINQ);
+    }
+
+
 }
