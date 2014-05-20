2202306fd2257bee371c462f4c22ebc6be86c516
Maylanie MESNIER <lyameina@gmail.com>
2014-05-20 09:44:34 +0200

    Marche plus... > vichier sivox_conf introuvable.... greuh

--- src/jeu/intro/Intro.java
+++ src/jeu/intro/Intro.java
@@ -1,5 +1,9 @@
 package jeu.intro;
 
+import devintAPI.DevintFrameListener;
+import devintAPI.FenetreAbstraite;
+import jeu.global.couleurs.Couleurs;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
@@ -9,7 +13,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
-public class Intro {
+public class Intro extends DevintFrameListener{
     //liste des images:
     private final String img1 = "ressources/images/intro/image1-fini.png";
     private final String img2 = "ressources/images/intro/image2-fini.png";
@@ -22,57 +26,82 @@ public class Intro {
     private JLabel image;
 
 
-    // appel au constructeur de la classe mère
-    public Intro() throws Exception{
 
+    // appel au constructeur de la classe mère
+    public Intro(String title) throws Exception{
+
+        super(title);
         String path = img1;
         BufferedImage image = ImageIO.read(new File(path));
         Image contentPane = new Image(image);
 
-        JFrame f = new JFrame();
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setContentPane(contentPane);
 
-        f.setSize(842, 596);
-        f.setLocation(100, 100);
-        f.setResizable(false);
 
-        f.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(contentPane);
+
+
+        this.setLocation(100, 100);
+        this.setResizable(false);
+
+        this.setVisible(true);
 
 
 
         Thread.sleep(2000); // suspendu pendant 2 seconde (chiffre en millisecondes)
         contentPane = new Image(ImageIO.read(new File(img2)));
-        f.setContentPane(contentPane);
-        f.revalidate();
-        f.repaint();
+        this.setContentPane(contentPane);
+        this.revalidate();
+        this.repaint();
 
 
         Thread.sleep(2000); // suspendu pendant 2 seconde (chiffre en millisecondes)
         contentPane = new Image(ImageIO.read(new File(img3)));
-        f.setContentPane(contentPane);
-        f.revalidate();
-        f.repaint();
+        this.setContentPane(contentPane);
+        this.revalidate();
+        this.repaint();
 
 
         Thread.sleep(2000); // suspendu pendant 2 seconde (chiffre en millisecondes)
         contentPane = new Image(ImageIO.read(new File(img4)));
-        f.setContentPane(contentPane);
-        f.revalidate();
-        f.repaint();
+        this.setContentPane(contentPane);
+        this.revalidate();
+        this.repaint();
 
 
         Thread.sleep(2000); // suspendu pendant 2 seconde (chiffre en millisecondes)
         contentPane = new Image(ImageIO.read(new File(img5)));
-        f.setContentPane(contentPane);
-        f.revalidate();
-        f.repaint();
+        this.setContentPane(contentPane);
+        this.revalidate();
+        this.repaint();
 
 
 
         Thread.sleep(2000); // suspendu pendant 2 seconde (chiffre en millisecondes)
 
         //fermer la fenetre et appeller une autre fenetre.
+    }
+
+    /**
+     * Pour modifier les couleurs de fond et de premier plan de la fenêtre Cette
+     * fonction est appelée par la fonction "changeColor" de la classe
+     * "Preferences" à chaque fois que l'on presse F3 on change la couleur du
+     * texte principal
+     *
+     */
+    @Override
+    public void changeColor() {}
+
+    // renvoie le fichier wave contenant le message d'accueil
+    @Override
+    protected String wavAccueil() {
+        return "../ressources/sons/apprentissage/accueil.wav";
+    }
+
+    // renvoie le fichier wave contenant la règle du jeu
+    @Override
+    protected String wavRegleJeu() {
+        return "../ressources/sons/apprentissage/aideF1.wav";
     }
 
 }
