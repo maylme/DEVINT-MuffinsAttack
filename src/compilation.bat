005d142b4ca327f11374045c35ba93664fc47d90
Jean-Christophe ISOARD <isoard@polytech.unice.fr>
2014-05-28 17:43:39 +0200

    Ajout du fichier Dossier_Devint et mise à jour du compilateur

--- src/compilation.bat
+++ src/compilation.bat
@@ -1,5 +1,5 @@
 rem set PATH=../../lib/;../../jre/bin/
 rem javac -cp .;../../VocalyzeSIVOX/bin/SI_VOX.jar -Djava.library.path=../ressources/lib/ -d ../bin devintAPI/*.java jeu/*.java
 
-javac -cp .;../../VocalyzeSIVOX/bin/SI_VOX.jar -d ../bin devintAPI/*.java jeu/*.java  
+javac -cp .;../../VocalyzeSIVOX/bin/SI_VOX.jar;../ressources/lib/jdom-2.0.5.jar;../ressources/lib/jl1.0.1.jar -d ../bin devintAPI/*.java jeu/*.java  
 pause
